e074f5df8731da5a7073dd2bc3fde99c0a8dbf11
Mert Yaşin <mert.yasin@gmail.com>
2017-10-17 18:28:33 +0300

    Semantic UI <Responsive/> SSR BUG: 'window is not defined'

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import Head from 'next/head'
 import Logo from '../components/logo'
+import {Responsive} from 'semantic-ui-react';
 
 export default () => (
-  <div className="root">
-    <Head>
-      <meta charSet="utf-8"/>
-      <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
-      <meta name="viewport" content="width=device-width, initial-scale=1"/>
-      <title>Next.js on Heroku</title>
-    </Head>
-    <style jsx>{`
+    <div className="root">
+        <Head>
+            <meta charSet="utf-8"/>
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <title>Next.js on Heroku</title>
+        </Head>
+        <style jsx>{`
       .root {
         font-family: sans-serif;
         line-height: 1.33rem;
@@ -24,12 +25,23 @@ export default () => (
       }
     `}</style>
 
-    <h1><Logo style={{ height: '1.45rem' }}/> Next.js on Heroku</h1>
+        <h1><Logo style={{height: '1.45rem'}}/> Next.js on Heroku</h1>
 
-    <p>Deploy <a href="https://zeit.co/blog/next">Next.js</a> universal web apps on <a href="https://www.heroku.com/home">Heroku</a>.</p>
+        <p>Deploy <a href="https://zeit.co/blog/next">Next.js</a> universal web apps on <a
+            href="https://www.heroku.com/home">Heroku</a>.</p>
 
-    <p>This <strong>demo deployment on Heroku</strong> is from the repo <a href="https://github.com/mars/heroku-nextjs">mars/heroku-nextjs</a>.</p>
+        <p>This <strong>demo deployment on Heroku</strong> is from the repo <a
+            href="https://github.com/mars/heroku-nextjs">mars/heroku-nextjs</a>.</p>
 
-    <p><a href="https://github.com/mars/heroku-nextjs/archive/master.zip">Download this Next.js app</a> as a Heroku-ready template, or follow <a href="https://github.com/mars/heroku-nextjs#production-deployment">Production Deployment</a> to push an existing app to Heroku.</p>
-  </div>
+        <p><a href="https://github.com/mars/heroku-nextjs/archive/master.zip">Download this Next.js app</a> as a
+            Heroku-ready template, or follow <a href="https://github.com/mars/heroku-nextjs#production-deployment">Production
+                Deployment</a> to push an existing app to Heroku.</p>
+
+        <Responsive maxWidth={Responsive.onlyTablet.minWidth}>
+            Visible if mobile
+        </Responsive>
+        <Responsive minWidth={Responsive.onlyTablet.minWidth}>
+            Visible if not mobile
+        </Responsive>
+    </div>
 )
